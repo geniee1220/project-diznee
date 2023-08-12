f6739795075886b7f5f68c0a3137138db74fbc72
geniee1220 <geniee1220@gmail.com>
2023-08-12 14:16:51 +0900

    Refactor : 사용하지 않는 코드 삭제

--- src/components/Nav/Nav.styles.js
+++ src/components/Nav/Nav.styles.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
 export const NavWrapper = styled.nav`
-  /* position: fixed; */
   position: sticky;
   top: 0;
   left: 0;
   right: 0;
   height: 70px;
-  /* background-color: ${(props) => (props.isShow ? '#090b13' : 'transparent')};
-   */
   background-color: #090b13;
   display: flex;
   justify-content: space-between;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,21 +3,6 @@ import Logo from '../Logo';
 import * as S from './Nav.styles';
 
 function Nav() {
-  //   const [isShow, setIsShow] = useState(false);
-
-  //   useEffect(() => {
-  //     window.addEventListener('scroll', () => {
-  //       if (window.scrollY > 50) {
-  //         setIsShow(true);
-  //       } else {
-  //         setIsShow(false);
-  //       }
-  //     });
-  //     return () => {
-  //       window.removeEventListener('scroll', () => {});
-  //     };
-  //   }, []);
-
   return (
     <S.NavWrapper>
       <Logo />
