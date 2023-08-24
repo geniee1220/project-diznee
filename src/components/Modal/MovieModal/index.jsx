98bf742853d474fc3c6f0f0c962e7b215aed1b60
geniee1220 <geniee1220@gmail.com>
2023-08-24 13:21:08 +0900

    Design : MovieModal UI 구현

--- src/components/Modal/MovieModal/index.jsx
+++ src/components/Modal/MovieModal/index.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import './MovieModal.css';
 
 function MovieModal({
@@ -10,7 +11,36 @@ function MovieModal({
   vote_average,
   setIsModalOpen,
 }) {
-  return <div>영화 모달 </div>;
+  const ref = useRef();
+
+  return (
+    <div className="presentation" role="presentation">
+      <div className="wrapper-modal">
+        <div className="modal" ref={ref}>
+          <span onClick={() => setIsModalOpen(false)} className="modal-close">
+            X
+          </span>
+
+          <img
+            className="modal__poster-img"
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+            alt="modal-img"
+          />
+
+          <div className="modal__content">
+            <p className="modal__details">
+              <span className="modal__user_perc">100% for you</span>{' '}
+              {release_date ? release_date : first_air_date}
+            </p>
+
+            <h2 className="modal__title"> {title ? title : name}</h2>
+            <p className="modal__overview">평점: {vote_average}</p>
+            <p className="modal__overview">{overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MovieModal;
