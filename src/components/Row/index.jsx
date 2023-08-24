d4fe39e1a4d743549413b1f1986578d06665e9d6
geniee1220 <geniee1220@gmail.com>
2023-08-24 12:51:06 +0900

    Feat : Row 컴포넌트, 슬라이드 기능

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import instance from '../../api/axios';
 
-function Row() {
-  return <div></div>;
+import './Row.css';
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
+
+function Row({ title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await instance.get(fetchUrl);
+    setMovies(response.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
+
+  console.log('id', document.getElementById(id));
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div
+          className="slider__arrow-left"
+          onClick={() => {
+            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+          }}
+        >
+          <span className="arrow">
+            <RiArrowLeftSLine />
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div
+          className="slider__arrow-right"
+          onClick={() => {
+            document.getElementById(id).scrollLeft += window.innerWidth - 80;
+          }}
+        >
+          <span className="arrow">
+            <RiArrowRightSLine />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Row;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,7 @@
+import requests from '../api/request';
 import Banner from '../components/Banner';
 import Category from '../components/Category';
+import Row from '../components/Row';
 import MainTemplates from '../components/Templates/MainTemplates';
 
 function MainPage() {
@@ -7,6 +9,18 @@ function MainPage() {
     <MainTemplates>
       <Banner />
       <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </MainTemplates>
   );
 }
