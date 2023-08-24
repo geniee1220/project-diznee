3528b83b0bdabba361f981baf8ad846526ead3de
geniee1220 <geniee1220@gmail.com>
2023-08-24 13:07:04 +0900

    Feat : MovieModal 컴포넌트

---
+++ src/components/Modal/MovieModal/index.jsx
@@ -0,0 +1,16 @@
+import './MovieModal.css';
+
+function MovieModal({
+  backdrop_path,
+  title,
+  overview,
+  name,
+  release_date,
+  first_air_date,
+  vote_average,
+  setIsModalOpen,
+}) {
+  return <div>영화 모달 </div>;
+}
+
+export default MovieModal;
