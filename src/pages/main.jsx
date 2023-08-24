5050e1eb61374b5cb7fcf631bd61c220a955fbd0
geniee1220 <geniee1220@gmail.com>
2023-08-24 12:12:14 +0900

    Feat : Category 컴포넌트

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,11 +1,12 @@
 import Banner from '../components/Banner';
+import Category from '../components/Category';
 import MainTemplates from '../components/Templates/MainTemplates';
 
 function MainPage() {
   return (
     <MainTemplates>
-      <div>메인 페이지</div>
       <Banner />
+      <Category />
     </MainTemplates>
   );
 }
