dc68a4bdcd6b3ae3cf84c592c17dd1270ff1f2a3
geniee1220 <geniee1220@gmail.com>
2023-08-25 07:41:00 +0900

    Feat : 내비게이션 UI 및 쿼리스트링 검색 기능 구현

--- src/components/Nav/Nav.styles.js
+++ src/components/Nav/Nav.styles.js
@@ -13,3 +13,29 @@ export const NavWrapper = styled.nav`
   padding: 0 36px;
   letter-spacing: 16px;
 `;
+
+export const LoginButton = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+export const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,39 @@
-import { useEffect, useState } from 'react';
 import Logo from '../Logo';
+
+import { useEffect, useState } from 'react';
+
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import * as S from './Nav.styles';
 
 function Nav() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const [searchValue, setSearchValue] = useState('');
+
+  // 쿼리스트링을 검색창에 표시
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <S.NavWrapper>
       <Logo />
+
+      {pathname === '/' ? (
+        <S.LoginButton>Login</S.LoginButton>
+      ) : (
+        <S.Input
+          type="text"
+          className="nav__input"
+          spellCheck="false"
+          placeholder="검색"
+          value={searchValue}
+          onChange={handleSearch}
+        />
+      )}
     </S.NavWrapper>
   );
 }
