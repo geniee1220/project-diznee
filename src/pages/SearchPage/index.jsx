2176d81f09cea210c18336b6ebe116f5c89df4ee
geniee1220 <geniee1220@gmail.com>
2023-08-26 07:46:00 +0900

    Feat : 검색 페이지 UI 구현

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import instance from '../../api/axios';
+
+import './SearchPage.css';
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
+
+  // 쿼리스트링 가져오기
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  const query = useQuery();
+  const searchTerm = query.get('q');
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchResult(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchResult = async () => {
+    try {
+      const response = await instance.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.results);
+
+      console.log(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (searchResults.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+            const movieImageUrl =
+              'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  className="movie__column-poster"
+                  onClick={() => navigate(`/${movie.id}`)}
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    );
+  } else {
+    return (
+      <section className="no-results">
+        <p className="no-results__text">
+          찾고자 하는 검색어 "{searchTerm}"의 검색 결과가 없습니다.
+        </p>
+      </section>
+    );
+  }
 }
 
 export default SearchPage;
