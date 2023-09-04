2dcb50fc51df0db887f0ec3ebb6922bde7509649
geniee1220 <geniee1220@gmail.com>
2023-09-05 07:54:00 +0900

    Chore : Api key 리셋

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
-  param: {
-    api_key: 'f754b3d9eebf8bb3f45da4d0df798d65',
+  params: {
+    api_key: '',
     language: 'ko-KR',
   },
 });
