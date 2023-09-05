1957f697c9c61ad7be820643160e26e3cbca2519
geniee1220 <geniee1220@gmail.com>
2023-09-05 09:06:53 +0900

    Feat : 구글 로그인 구현

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,16 +1,36 @@
 import Logo from '../Logo';
 
 import { useEffect, useState } from 'react';
-
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 
 import * as S from './Nav.styles';
 
 function Nav() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
   const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        if (pathname === '/') navigate('/main');
+      } else {
+        navigate('/');
+      }
+    });
+  }, []);
 
   // 쿼리스트링을 검색창에 표시
   const handleSearch = (e) => {
@@ -18,12 +38,22 @@ function Nav() {
     navigate(`/search?q=${e.target.value}`);
   };
 
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <S.NavWrapper>
       <Logo />
 
       {pathname === '/' ? (
-        <S.LoginButton>Login</S.LoginButton>
+        <S.LoginButton onClick={handleAuth}>Login</S.LoginButton>
       ) : (
         <S.Input
           type="text"
