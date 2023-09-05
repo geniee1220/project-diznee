4095ac0b853461e6e17d86b27cc58d5eb2bf705a
geniee1220 <geniee1220@gmail.com>
2023-09-05 09:07:08 +0900

    Design : 커서 포인터 추가

--- src/components/Nav/Nav.styles.js
+++ src/components/Nav/Nav.styles.js
@@ -21,6 +21,7 @@ export const LoginButton = styled.a`
   letter-spacing: 1.5px;
   border: 1px solid #f9f9f9;
   transition: all 0.2s ease 0s;
+  cursor: pointer;
 
   &:hover {
     background-color: #f9f9f9;
