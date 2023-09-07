68ad7c46862db1af8c6a55cfd91738ad8428ed4a
geniee1220 <geniee1220@gmail.com>
2023-09-07 15:46:36 +0900

    Feat : User Reducer 생성

---
+++ src/store/userSlice.js
@@ -0,0 +1,28 @@
+import createSlice from '@reduxjs/toolkit/createSlice';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    id: '',
+    email: '',
+    photoURL: '',
+    displayName: '',
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.id = action.payload.id;
+      state.email = action.payload.email;
+      state.photoURL = action.payload.photoURL;
+      state.displayName = action.payload.displayName;
+    },
+    removeUSer: (state) => {
+      state.id = '';
+      state.email = '';
+      state.photoURL = '';
+      state.displayName = '';
+    },
+  },
+});
+
+export const { setUser, removeUSer } = userSlice.actions;
+export default userSlice.reducer;
