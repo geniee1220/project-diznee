daeea426bed248c7a93b12ba265441ba543d3f6d
geniee1220 <geniee1220@gmail.com>
2023-09-07 15:48:22 +0900

    Feat : Redux Provider 설정

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import app from './firebase';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
